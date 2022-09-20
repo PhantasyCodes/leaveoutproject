e139c5123ba2d3f5c22553d985849a7fbc49b48c
Michelle-Kabui <98522361+Michelle-Kabui@users.noreply.github.com>
2022-09-20 21:39:28 +0300

    Add files via upload

---
+++ admin_form.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <body>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<form action="#" method="post" >
+                <fieldset>
+                    <div class="form-name">
+                        <label>Registration Form</label>
+                    </div>
+                    <div class="field">
+                        <label for="full_name">Full Name :</label>
+                        <input type="text" name="full_name" required>
+                    </div>
+                    <div class="field">
+                        <label for="email">Email :</label>
+                        <input type="text" name="email" required>
+                        
+                    </div>
+                    <div class="field">
+                        <label for="department">Department :</label>
+                        <input type="text" name="department" required>
+                        
+                    </div>
+                    <div class="field">
+                        <label for="role">Role :</label>
+                        <input type="text" name="role" required>
+                    </div>
+                    <input type="submit" name="register" value="Register Now" form="register">
+                </fieldset>
+            </form>
+    </body>
+</html>
