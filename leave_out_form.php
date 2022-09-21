31321e6e62e1fa06e1c9e7a39922b1839cfd445f
David King <davidkingmwai@gmail.com>
2022-09-21 17:18:31 +0300

    Added Leave Out Form

--- leave_out_form.php
+++ leave_out_form.php
@@ -4,9 +4,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
-    <div></div>
+    <div class="form-container">
+        <form class="input-form" action="">
+            <h1>Leave Out Form</h1>
+            <div class="form-input">
+                <label for="id">Employee Id:</label>
+                <input name="id" type="text">
+            </div>
+            <div class="form-input">
+                <label for="type">Leave Type:</label>
+                <select name="type">
+                    <option value=""></option>
+                    <option value="">Maternity Leave</option>
+                    <option value="">Paternity Leave</option>
+                    <option value="">Casual Leave</option>
+                    <option value="">Earned Leave</option>
+                    <option value="">Compensatory Off</option>
+                    <option value="">Sick Leave</option>
+                </select>
+            </div>
+            <div class="form-input">
+                <label for="start">Start Date:</label>
+                <input name="start" type="date">
+            </div>
+            <div class="form-input">
+                <label for="end">End Date:</label>
+                <input name="end" type="date">
+            </div>
+            <div class="form-input">
+                <label for="comment">Comment:</label>
+                <input name="comment" type="text">
+            </div>
+        </form>
+    </div>
 </body> 
 </html>
